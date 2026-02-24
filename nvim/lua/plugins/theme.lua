a540e044709d9320026da8c895064dcd9cf97628
Igor Savurov <savurov@gmail.com>
2026-02-24 03:41:21 +0200

    nvim: change theme to vscode-like

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,22 +1,21 @@
 
-  return { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+return {
+  'Mofiqul/vscode.nvim',
+  priority = 1000,
+  config = function()
+    require('vscode').setup {
+      style = 'dark',
+      italic_comments = false,
+    }
+    vim.cmd.colorscheme 'vscode'
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  }
+    -- Align Python keyword coloring closer to VSCode (with/as/in -> Keyword).
+    local link = function(from, to)
+      vim.api.nvim_set_hl(0, from, { link = to })
+    end
+    link('@keyword.python', 'Keyword')
+    link('@keyword.operator.python', 'Keyword')
+    link('@keyword.import.python', 'Keyword')
+    link('@lsp.type.keyword.python', 'Keyword')
+  end,
+}
