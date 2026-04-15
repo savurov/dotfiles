38c97bd781ed310ebdaa75f0528582c85523928f
Igor Savurov <savurov@gmail.com>
2026-04-15 21:27:51 +0300

    nvim: update lsp restart for 0.12v

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -43,7 +43,7 @@ map('n', '<C-j>', '<C-w><C-j>', 'Move focus down')
 map('n', '<C-k>', '<C-w><C-k>', 'Move focus up')
 map('v', '<C-c>', '"+y', 'ctrl+c')
 map('n', '<leader>q', '<cmd>qa<CR>', 'Quit')
-map('n', '<leader>lr', '<cmd>LspRestart<CR>', 'Restart LSP')
+map('n', '<leader>lr', '<cmd>lsp restart<CR>', 'Restart LSP')
 map('n', '<leader>lt', autocomplete.toggle, 'Toggle autocomplete')
 
 --====== Neo-tree  =====
